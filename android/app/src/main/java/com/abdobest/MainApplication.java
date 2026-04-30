868977ea74ac40f27b486ade546946b3d7f92ef1
Abdelrahman <abdo_1111400@yahoo.com>
2026-04-30 08:35:13 +0300

    fix: align all files to RN 0.72.17

--- android/app/src/main/java/com/abdobest/MainApplication.java
+++ android/app/src/main/java/com/abdobest/MainApplication.java
@@ -5,15 +5,13 @@ import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
-import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
     private final ReactNativeHost mReactNativeHost =
-            new DefaultReactNativeHost(this) {
+            new ReactNativeHost(this) {
                 @Override
                 public boolean getUseDeveloperSupport() {
                     return BuildConfig.DEBUG;
@@ -33,7 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
 
                 @Override
                 protected boolean isNewArchEnabled() {
-                    return true;   // New Architecture enabled
+                    return false;
                 }
 
                 @Override
@@ -51,10 +49,5 @@ public class MainApplication extends Application implements ReactApplication {
     public void onCreate() {
         super.onCreate();
         SoLoader.init(this, /* native exopackage */ false);
-        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            // If you opted-in for the New Architecture, we load the native entry point for this app.
-            DefaultNewArchitectureEntryPoint.load();
-        }
-        // Flipper is removed – it was only for debugging and not needed in release builds.
     }
 }
