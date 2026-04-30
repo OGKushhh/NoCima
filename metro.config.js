9b0171baf22836b03dd6fc5464d72ba1871c0b8f
Abdelrahman <abdo_1111400@yahoo.com>
2026-04-30 11:45:53 +0300

    Fix: remove broken metro.config.js

--- metro.config.js
+++
@@ -1,5 +0,0 @@
-const {getDefaultConfig, mergeConfig} = require('metro-config');
-
-const defaultConfig = getDefaultConfig(__dirname);
-const config = {};
-module.exports = mergeConfig(defaultConfig, config);
